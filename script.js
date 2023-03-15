38722a9abbfd019d50f2ca588001a56251fea494
Boat"   Sasipatr Siripairoj <125846563+sspsi77@users.noreply.github.com>
2023-03-16 00:21:40 +0700

    Update script.js
    add Exercise #3

--- script.js
+++ script.js
@@ -46,3 +46,52 @@ for(let i=1; i<=5; i++){
    console.log(stars);
     }
 }
+
+// Exercise #3
+
+// Repeatedly print the value of the variable xValue, decreasing it by 0.5 each time,
+// as long as xValue remains positive
+
+let xValue = 7;
+while (xValue >= 0) {
+    console.log(xValue);
+    xValue = xValue - 0.5;
+}
+
+// Print all the odd numbers between 1 - 100
+
+for(let i=0; i < 100; i++) {
+    if ( i%2 === 1 ) {
+        console.log(i);
+    }
+}
+
+// Write a method with a while loop to print 1 through n in square brackets. 
+// For example, if n = 6 print [1] [2] [3] [4] [5] [6]
+let num = '';
+ const n_1 = 6;
+ let i = 1;
+ while ( i <= n_1 ) {
+    num = num + '[' + i + '] ';
+    i++;
+ }
+
+ console.log(num);
+
+
+// Write a method with a while loop that computes the sum of first n positive integers: 
+// sum = 1 + 2 + 3 + … + n
+// Examples:
+// n = 5 sum = 15
+// n = 19 sum = 190
+
+//part 4
+let sum = 0;
+const n_2 = 5;
+let j = 0;
+while (j <= n_2) {
+   sum = sum + j ;
+   j++;
+} 
+
+console.log(sum);
